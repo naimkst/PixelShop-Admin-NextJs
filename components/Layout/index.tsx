2fe93c7e871e06360a0576ce3fdf16cbb4609af2
naimkst <najmulspark@gmail.com>
2022-05-27 01:30:06 +0600

    first commit

---
+++ components/Layout/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Navigation from '../Navigation';
+import Sidebar from '../Sidebar'
+
+export default function Layout({ children }) {
+
+  return (
+    <>
+      <div className="w-full h-full bg-[#f9f9f9]">
+        <div className="flex flex-no-wrap">
+          {/* Sidebar starts */}
+
+              <Sidebar />
+
+          {/* Sidebar ends */}
+          <div className="w-full">
+            {/* Navigation starts */}
+
+            <Navigation />
+
+            {/* Navigation ends */}
+            {/* Remove class [ h-64 ] when adding a card block */}
+            <div className="container mx-auto py-10 md:w-4/5 w-11/12 px-6">
+              {/* Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border */}
+              <div className="w-full h-full rounded border-gray-300">
+                { children }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
