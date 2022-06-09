7ecab559a224eabb9214303839537288804375b0
naimkst <najmulspark@gmail.com>
2022-06-10 01:10:48 +0600

    Product Edit Done

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -5,7 +5,6 @@ import { URL } from "url";
 import { getAlltData, deleteById } from "../../services/product";
 import { ToastContainer, toast } from "react-toastify";
 
-
 function Index() {
   const [show, setShow] = useState(null);
   const [Product, setProduct] = useState([]);
@@ -25,15 +24,14 @@ function Index() {
   };
 
   const deleteHandle = async (id: number) => {
-      try {
-        const response = await deleteById(id);
-        console.log(response);
-        setProduct(response);
-        toast.success("Delete Successfully");
-      } catch (error) {
-        toast.error("Something Wrong");
-      }
-
+    try {
+      const response = await deleteById(id);
+      console.log(response);
+      setProduct(response);
+      toast.success("Delete Successfully");
+    } catch (error) {
+      toast.error("Something Wrong");
+    }
   };
 
   useEffect(() => {
@@ -70,6 +68,7 @@ function Index() {
                 <th className="font-normal text-left pl-16">Action</th>
               </tr>
             </thead>
+
             <tbody className="w-full">
               {Product?.map((prd: any) => (
                 <tr
@@ -104,23 +103,28 @@ function Index() {
                   </td>
                   <td className="pl-16">
                     <div className="flex items-center gap-2">
-                      <span className=" cursor-pointer">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-6 w-6"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          strokeWidth={2}
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                          />
-                        </svg>
-                      </span>
-                      <span className=" cursor-pointer" onClick={() => deleteHandle(prd.id)}>
+                      <Link href={`/products/${prd.id}`}>
+                        <span className=" cursor-pointer">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                            />
+                          </svg>
+                        </span>
+                      </Link>
+                      <span
+                        className=" cursor-pointer"
+                        onClick={() => deleteHandle(prd.id)}
+                      >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           className="h-6 w-6"
@@ -142,6 +146,29 @@ function Index() {
               ))}
             </tbody>
           </table>
+
+          {isLoading ? (
+            <div className="flex justify-center w-full text-center content-evenly items-center mt-5 m-auto py-20">
+              <svg
+                role="status"
+                className="w-20 h-20 text-center flex items-center justify-center mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </>
--- services/product/index.tsx
+++ services/product/index.tsx
@@ -19,4 +19,16 @@ export const deleteById = async (id: number) => {
   return data;
 };
 
+//Find By ID
+export const getById = async (id: any) => {
+  const { data } = await request.get(`/product/find/${id}`);
+  return data;
+};
+
+//Product Update
+export const productUpdate = async (id: any, payload: any) => {
+  const { data } = await request.post(`/product/update/${id}`, payload);
+  return data;
+};
+
 
